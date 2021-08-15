fceabdeaa799c37b68ecbb524a3b0fadd6574d53
NobinKhan <nobin118960@gmail.com>
2021-08-15 09:57:52 +0600

    requirements and settings.py minor update

--- IronMarket/settings.py
+++ IronMarket/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -55,7 +54,7 @@ ROOT_URLCONF = 'IronMarket.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "templates")],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,7 +120,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS= [
-    os.path.join(BASE_DIR, 'static')
+    BASE_DIR, 'static'
 ]
 
 # Default primary key field type
