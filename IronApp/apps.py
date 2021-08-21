c19dd7feacffcfd43ece7778fefef967441c4f7a
NobinKhan <nobin118960@gmail.com>
2021-08-21 17:01:11 +0600

    basic changes

--- IronApp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class IronappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'IronApp'
--- IronMarket/settings.py
+++ IronMarket/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles', 
-    'IronApp',
+    'index.apps.IndexConfig',
+    
 ]
 
 MIDDLEWARE = [
