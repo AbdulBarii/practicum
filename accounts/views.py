18885c038a8abd53a67609692f146a73223b4fec
NobinKhan <nobin118960@gmail.com>
2021-08-28 17:48:34 +0600

    login and registration is fully functional now

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
-from django.contrib import messages, auth
+from django.contrib import auth, messages
 from django.contrib.auth.models import Group
+from django.shortcuts import redirect, render
+
 from .models import CustomUser
 
 
@@ -15,7 +16,8 @@ def login(request):
             loginpassword = request.POST['password']
 
             # validating user information
-            user = auth.authenticate(username=loginusername, password=loginpassword)
+            user = auth.authenticate(
+                username=loginusername, password=loginpassword)
 
             # saving user information
             if user is not None:
@@ -23,7 +25,8 @@ def login(request):
                 messages.success(request, "You have successfully logged in")
                 return redirect('dashboard')
             else:
-                messages.error(request, "Username or Password is not matched. Please try again !")
+                messages.error(
+                    request, "Username or Password is not matched. Please try again !")
                 return redirect(request.META['HTTP_REFERER'])
     except:
         return redirect('index')
@@ -32,7 +35,6 @@ def login(request):
 def register(request):
     if request.method == 'POST':
         # Get form values
-        group_check = int(request.POST['group'])
         first_name = request.POST['first_name']
         last_name = request.POST['last_name']
         username = request.POST['username']
@@ -44,30 +46,28 @@ def register(request):
         if password == password2:
             # Check username
             if CustomUser.objects.filter(username=username).exists():
-                messages.error(request,'The username is already taken')
+                messages.error(request, 'The username is already taken')
                 return redirect('register')
             else:
-            # Check email
+                # Check email
                 if CustomUser.objects.filter(email=email).exists():
-                    messages.error(request,'The email is already taken')
+                    messages.error(request, 'The email is already taken')
                     return redirect('register')
                 else:
-                    #Looks good
-                    user = CustomUser.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
+                    # Looks good
+                    user = CustomUser.objects.create_user(
+                        username=username, password=password, email=email, first_name=first_name, last_name=last_name)
                     user.save()
-                    if group_check == 2:
-                        group = Group.objects.get(name='Home_owner')
-                    else:
-                        group = Group.objects.get(name='user')
-                    
+                    group = Group.objects.get(name='user')
                     user.groups.add(group)
-                    messages.success(request, 'You are now registered and can log in')
+                    messages.success(
+                        request, 'Registration success')
                     return redirect('login')
         else:
             messages.error(request, 'Passwords do not match')
             return redirect('accounts/register.html')
     else:
-        return render(request,'accounts/register.html')
+        return render(request, 'accounts/register.html')
 
 
 def logout(request):
